26c58343cc977c6561db3b35734349e346c48125
angel <ponxosio@gmail.com>
2017-06-18 21:30:26 +0200

    turbidostat test done !

--- tests/auto/blocklymachinev1test/blocklymachinev1test.pro
+++ tests/auto/blocklymachinev1test/blocklymachinev1test.pro
@@ -1,3 +1,18 @@
+# ensure one "debug_and_release" in CONFIG, for clarity...
+debug_and_release {
+    CONFIG -= debug_and_release
+    CONFIG += debug_and_release
+}
+    # ensure one "debug" or "release" in CONFIG so they can be used as
+    #   conditionals instead of writing "CONFIG(debug, debug|release)"...
+CONFIG(debug, debug|release) {
+    CONFIG -= debug release
+    CONFIG += debug
+}
+CONFIG(release, debug|release) {
+    CONFIG -= debug release
+    CONFIG += release
+}
 
 QT += testlib
 QT -= gui
@@ -8,3 +23,54 @@ CONFIG -= app_bundle
 TEMPLATE = app
 
 SOURCES +=  tst_blocklymachinev1test.cpp
+
+debug {
+    INCLUDEPATH += X:\fluidicMachineModel\dll_debug\include
+    LIBS += -L$$quote(X:\fluidicMachineModel\dll_debug\bin) -lFluidicMachineModel
+
+    INCLUDEPATH += X:\constraintsEngine\dll_debug\include
+    LIBS += -L$$quote(X:\constraintsEngine\dll_debug\bin) -lconstraintsEngineLibrary
+
+    INCLUDEPATH += X:\utils\dll_debug\include
+    LIBS += -L$$quote(X:\utils\dll_debug\bin) -lutils
+
+    INCLUDEPATH += X:\commomModel\dll_debug\include
+    LIBS += -L$$quote(X:\commomModel\dll_debug\bin) -lcommonModel
+
+    INCLUDEPATH += X:\protocolGraph\dll_debug\include
+    LIBS += -L$$quote(X:\protocolGraph\dll_debug\bin) -lprotocolGraph
+
+    INCLUDEPATH += X:\blockly_fluidicMachine_translator\dll_debug\include
+    LIBS += -L$$quote(X:\blockly_fluidicMachine_translator\dll_debug\bin) -lblocklyFluidicMachineTranslator
+}
+
+!debug {
+    INCLUDEPATH += X:\fluidicMachineModel\dll_release\include
+    LIBS += -L$$quote(X:\fluidicMachineModel\dll_release\bin) -lFluidicMachineModel
+
+    INCLUDEPATH +=X:\constraintsEngine\dll_release\include
+    LIBS += -L$$quote(X:\constraintsEngine\dll_release\bin) -lconstraintsEngineLibrary
+
+    INCLUDEPATH += X:\utils\dll_release\include
+    LIBS += -L$$quote(X:\utils\dll_release\bin) -lutils
+
+    INCLUDEPATH += X:\commomModel\dll_release\include
+    LIBS += -L$$quote(X:\commomModel\dll_release\bin) -lcommonModel
+
+    INCLUDEPATH += X:\protocolGraph\dll_release\include
+    LIBS += -L$$quote(X:\protocolGraph\dll_release\bin) -lprotocolGraph
+
+    INCLUDEPATH += X:\blockly_fluidicMachine_translator\dll_release\include
+    LIBS += -L$$quote(X:\blockly_fluidicMachine_translator\dll_release\bin) -lblocklyFluidicMachineTranslator
+}
+
+INCLUDEPATH += X:\libraries\cereal-1.2.2\include
+INCLUDEPATH += X:\libraries\json-2.1.1\src
+
+INCLUDEPATH += X:\swipl\include
+LIBS += -L$$quote(X:\swipl\bin) -llibswipl
+LIBS += -L$$quote(X:\swipl\lib) -llibswipl
+
+RESOURCES += \
+    machines.qrc
+
--- tests/auto/blocklymachinev1test/tst_blocklymachinev1test.cpp
+++ tests/auto/blocklymachinev1test/tst_blocklymachinev1test.cpp
@@ -1,35 +1,91 @@
 #include <QtTest>
 
+#include <QtTest>
+#include <QTemporaryFile>
+#include <QFile>
+
+#include <memory>
+
 // add necessary includes here
 
-class blocklyMachineV1Test : public QObject
+#include <blocklyFluidicMachineTranslator/blocklyfluidicmachinetranslator.h>
+
+#include <commonmodel/plugininterface/pluginabstractfactory.h>
+
+class BlocklyMachineV1Test : public QObject
 {
     Q_OBJECT
 
+private:
+    void copyResourceFile(const QString & resourcePath, QTemporaryFile* file) throw(std::invalid_argument);
 public:
-    blocklyMachineV1Test();
-    ~blocklyMachineV1Test();
+    BlocklyMachineV1Test();
+    ~BlocklyMachineV1Test();
 
 private slots:
-    void test_case1();
+    void turbidostatMachineLayout();
 
 };
 
-blocklyMachineV1Test::blocklyMachineV1Test()
+BlocklyMachineV1Test::BlocklyMachineV1Test()
 {
 
 }
 
-blocklyMachineV1Test::~blocklyMachineV1Test()
+BlocklyMachineV1Test::~BlocklyMachineV1Test()
 {
 
 }
 
-void blocklyMachineV1Test::test_case1()
+void BlocklyMachineV1Test::turbidostatMachineLayout()
 {
+    QTemporaryFile* tempFile = new QTemporaryFile();
+    if (tempFile->open()) {
+        try {
+            copyResourceFile(":/machines/machines/turbidostat_test.json", tempFile);
 
+            std::shared_ptr<PluginAbstractFactory> factory;
+            BlocklyFluidicMachineTranslator translator(tempFile->fileName().toStdString(), factory);
+
+            std::shared_ptr<FluidicMachineModel> model = translator.translateFile();
+
+            std::string generatedMachine = model->getMachineGraph()->toString();
+            qDebug() << "generated machine:";
+            qDebug() << generatedMachine.c_str();
+
+            std::string expectedMachine = "digraph{0[label=\"0_OPEN_CONTAINER\"];1[label=\"1_CLOSE_CONTAINER\"];3[label=\"3_OPEN_CONTAINER\"];2[label=\"2_UNIDIRECTIONAL_PUMP\"];2->3[label=\" direction=Unknow, cutted=0\"];0->1[label=\" direction=Unknow, cutted=0\"];1->2[label=\" direction=Unknow, cutted=0\"];}";
+            qDebug() << "expected machine:";
+            qDebug() << expectedMachine.c_str();
+
+            QVERIFY2(generatedMachine.compare(expectedMachine) == 0, "generated machine and expected machine are not the same");
+
+        } catch (std::exception & e) {
+            delete tempFile;
+            QFAIL(e.what());
+        }
+    } else {
+        delete tempFile;
+        QFAIL("imposible to create temporary file");
+    }
+    delete tempFile;
 }
 
-QTEST_APPLESS_MAIN(blocklyMachineV1Test)
+void BlocklyMachineV1Test::copyResourceFile(const QString & resourcePath, QTemporaryFile* tempFile) throw(std::invalid_argument) {
+    QFile resourceFile(resourcePath);
+    if(!resourceFile.open(QIODevice::ReadOnly | QIODevice::Text)) {
+        throw(std::invalid_argument("imposible to open" + resourcePath.toStdString()));
+    }
+
+    QTextStream out(tempFile);
+
+    QTextStream in(&resourceFile);
+    while (!in.atEnd()) {
+        QString line = in.readLine();
+        out << line;
+    }
+    out.flush();
+}
+
+QTEST_APPLESS_MAIN(BlocklyMachineV1Test)
 
 #include "tst_blocklymachinev1test.moc"
